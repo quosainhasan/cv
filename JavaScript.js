f6afbf3ddbcd2d9b89028b0704d40a707790ae1b
quosainhasan <33932120+quosainhasan@users.noreply.github.com>
2022-08-10 00:33:24 +0530

    Add My first website which is HTML5, CSS3 and JS

---
+++ JavaScript.js
@@ -0,0 +1,42 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option Clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+
+    if(mode == 'black'){
+        document.getElementById('theme-style').href = 'black.css'
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
